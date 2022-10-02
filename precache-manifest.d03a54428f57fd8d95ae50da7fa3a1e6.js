6f1db424b85d13849e243367438c1bb0ff9f5c4e
Code Sandbox <hello@codesandbox.io>
2022-10-02 16:21:43 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d03a54428f57fd8d95ae50da7fa3a1e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5470ff328d5a167f7ea483d421340531",
+    "url": "https://zbndm.github.io/csb-ypise7/index.html"
+  },
+  {
+    "revision": "c16b1fdd5a657a29488c",
+    "url": "https://zbndm.github.io/csb-ypise7/static/css/main.16ff99fc.chunk.css"
+  },
+  {
+    "revision": "f0c4bbc54476d8225ae8",
+    "url": "https://zbndm.github.io/csb-ypise7/static/js/2.ef6ec90b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "https://zbndm.github.io/csb-ypise7/static/js/2.ef6ec90b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c16b1fdd5a657a29488c",
+    "url": "https://zbndm.github.io/csb-ypise7/static/js/main.64f1cd76.chunk.js"
+  },
+  {
+    "revision": "786ac71c3ea709177f41",
+    "url": "https://zbndm.github.io/csb-ypise7/static/js/runtime-main.b0270852.js"
+  },
+  {
+    "revision": "638be90904f9f9248d3c69cb2992c562",
+    "url": "https://zbndm.github.io/csb-ypise7/static/media/react.638be909.png"
+  }
+]);
